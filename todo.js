9f3e80a43f7ef9bf15c2df70227e46d6cb086164
ishiprak <42737064+ishiprak@users.noreply.github.com>
2019-03-17 17:55:32 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,15 +1,19 @@
 function add1(){
+	
 	//work.innerHTML="<br/><li>Hello</li>";
 	var np=document.getElementById("inp").value;
 	document.getElementById("wrk1").innerHTML+="<br/><li>"+np+"</li>";
 }
 
 function add2(){
+	
 	//work.innerHTML="<br/><li>Hello</li>";
 	var np=document.getElementById("inp").value;
 	document.getElementById("wrk2").innerHTML+="<br/><li>"+np+"</li>";
 }
+
 function add3(){
+	
 	//work.innerHTML="<br/><li>Hello</li>";
 	var np=document.getElementById("inp").value;
 	document.getElementById("wrk3").innerHTML+="<br/><li>"+np+"</li>";
@@ -21,16 +25,13 @@ function del(){
 }
 
 function cross(){
+	
 	event.target.style.textDecoration="line-through";
 }
 
 function orig(){
+	
 	event.target.style.textDecoration="none";
 }
-
-
-
-
-
 
 //document.getElementById("wrk").childNodes[0].addEventListener("click",sp2());
