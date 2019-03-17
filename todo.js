c40c0a70552937d41ca703990aab735655245889
ishiprak <42737064+ishiprak@users.noreply.github.com>
2019-03-17 21:12:42 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,35 +1,32 @@
 function add1(){
 
-	var val=swal("Add your work:", {
+	swal("Add your work:", {
     content: "input",
+    })
+    .then((value) => {
+     document.getElementById("wrk1").innerHTML+="<br/><li>"+ value +"</li>";
     });
-	
-	 /*.then((value) => {
-     swal(`You added: ${value}`);
-    });*/
-	//var np=document.getElementById("inp").value;
-
-	document.getElementById("wrk1").innerHTML+="<br/><li>"+val.content+"</li>";
 }
 
 function add2(){
 	
-	var val=swal("Add your work:", {
+	swal("Add your work:", {
     content: "input",
     })
     .then((value) => {
-     swal(`You added: ${value}`);
+     document.getElementById("wrk2").innerHTML+="<br/><li>"+ value +"</li>";
     });
-	document.getElementById("wrk2").innerHTML+="<br/><li>"+val.value+"</li>";
+	
 }
 
 function add3(){
 	
-	var val=swal("Add your work:", {
+	swal("Add your work:", {
     content: "input",
+    })
+    .then((value) => {
+     document.getElementById("wrk3").innerHTML+="<br/><li>"+ value +"</li>";
     });
-	//var np=document.getElementById("inp").value;
-	document.getElementById("wrk3").innerHTML+="<br/><li>"+val.vaue+"</li>";
 }
 
 function del(){
